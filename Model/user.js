57c522de15696e0043e6b74fdf20b02e99954216
pramudnadula <pramudn@gmail.com>
2022-11-06 22:16:39 +0530

    first commit

---
+++ Model/user.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+var ObjectId = require("bson").ObjectId;
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  fname: {
+    type: String,
+    required: true,
+  },
+  lname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
