fb0cbfa0e6b92c8ab85bb102834414af55a9345a
pramudnadula <pramudn@gmail.com>
2022-11-13 12:32:20 +0530

    edit view delete
    Signed-off-by: pramudnadula <pramudn@gmail.com>

--- Controller/marianResources.js
+++ Controller/marianResources.js
@@ -3,7 +3,7 @@ const MarianResources = require('../Model/marianResources');
 //add MarianResources
 exports.addMarianResources = async (req, res) => {
 
-    const { title, image, name, details, category, author, habit, lifeSpan } = req.body;
+    const { title, image, name, details, category, author, habit, lifeSpan, uid } = req.body;
     console.log(req.body);
     if (!image || !name || !details || !category) {
         return res.status(400).json({ message: "Please enter all fields" });
@@ -16,7 +16,8 @@ exports.addMarianResources = async (req, res) => {
         category,
         author,
         habit,
-        lifeSpan
+        lifeSpan,
+        uid
 
     });
     try {
@@ -43,6 +44,45 @@ exports.getMarianResources = async (req, res) => {
 exports.getOneMarianResources = async (req, res) => {
     try {
         const marianResources = await MarianResources.findById(req.params.id);
+        res.send({ message: "success", marianResources });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+//get all marianResources by object uid
+exports.getMarianResourcesByUid = async (req, res) => {
+    try {
+        uid = req.body.uid;
+        if (!uid) {
+            return res.status(400).json({ message: "Please enter all fields" });
+        }
+        const marianResources = await MarianResources.find(
+            { uid: uid }
+        );
+        res.send({ message: "success", marianResources: marianResources });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+//update MarianResources
+exports.updateMarianResources = async (req, res) => {
+    try {
+        const marianResources = await MarianResources.findByIdAndUpdate(req.params.id, req.body);
+        res.send({ message: "success", marianResources });
+    }
+    catch (err) {
+        res.status(400).send(err);
+    }
+}
+
+//delete MarianResources
+exports.deleteMarianResources = async (req, res) => {
+    try {
+        const marianResources = await MarianResources.findByIdAndDelete(req.params.id);
         res.send({ message: "success", marianResources });
     }
     catch (err) {
--- Routers/marianResources.js
+++ Routers/marianResources.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
-const { addMarianResources, getMarianResources, getOneMarianResources } = require("../Controller/marianResources");
+const { addMarianResources, getMarianResources, getOneMarianResources, getMarianResourcesByUid, updateMarianResources, deleteMarianResources } = require("../Controller/marianResources");
 
 router.post("/addMarianResources", addMarianResources);
 router.get("/getMarianResources", getMarianResources);
 router.get("/getOneMarianResources/:id", getOneMarianResources);
+router.post("/getMarianResourcesByUid", getMarianResourcesByUid);
+router.put("/updateMarianResources/:id", updateMarianResources);
+router.delete("/deleteMarianResources/:id", deleteMarianResources);
 
 module.exports = router;
