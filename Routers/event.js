4b85dcfa2cd03e3129c311512d66110a334dbb11
pramudnadula <pramudn@gmail.com>
2022-11-14 10:21:27 +0530

    events
    Signed-off-by: pramudnadula <pramudn@gmail.com>

--- Routers/event.js
+++ Routers/event.js
@@ -1,4 +1,4 @@
-const { addEvent, getMyEvents, deleteEvent, editEvent } = require("../Controller/event");
+const { addEvent, getMyEvents, deleteEvent, editEvent, getAllEvent } = require("../Controller/event");
 
 const router = require("express").Router();
 
@@ -8,6 +8,7 @@ router.post("/add", addEvent);
 router.post("/myevent", getMyEvents);
 router.post("/del", deleteEvent);
 router.post("/edit", editEvent)
+router.get('/allevent', getAllEvent)
 
 
 module.exports = router;
